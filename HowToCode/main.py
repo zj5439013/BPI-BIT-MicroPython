56db54e396254bd6ff21cfba42185701e43df517
Junhuan <Junhuanchen@qq.com>
2018-08-19 18:33:11 +0800

    Test

---
+++ HowToCode/main.py
@@ -0,0 +1,18 @@
+import network
+print('network smartconfig')
+network.smartconfig()
+
+import time
+time.sleep(3)
+
+import webdav
+print('webdav start')
+webdav.start()
+
+import os
+print('check index.py')
+os.stat('index.py')
+
+import reload
+print('reload start index.py')
+reload.start('index.py')
