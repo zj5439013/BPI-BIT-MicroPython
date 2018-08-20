b7b1a452bf856da509bd2bce49e75206620899c8
Junhuan <Junhuanchen@qq.com>
2018-08-20 09:24:56 +0800

    updated

--- HowToCode/main.py
+++ HowToCode/main.py
@@ -1,18 +1,1 @@
-import network
-print('network smartconfig')
-network.smartconfig()
-
-import time
-time.sleep(3)
-
-import webdav
-print('webdav start')
-webdav.start()
-
-import os
-print('check index.py')
-os.stat('index.py')
-
-import reload
-print('reload start index.py')
-reload.start('index.py')
+print("bpibit with you")
