f0cf19da1c293fb437cc39715f84dc9fd1b4144b
Juwan <32978053+junhuanchen@users.noreply.github.com>
2018-10-08 13:57:51 +0800

    add music.py unit_test.

--- HowToCode/10.MicroBit/gpio.py
+++ HowToCode/10.MicroBit/gpio.py
@@ -9,7 +9,14 @@ class input_output():
 	def write_digital(self, v):
 		pinx = Pin(self.pin, Pin.OUT).value(v)
 
+	def read_digital(self):
+		return Pin(self.pin, Pin.IN).value()
+
+	def read_analog(ATTN = ADC.ATTN_0DB):
+		return ADC(Pin(self.num, ATTN)).read()
+
+	def write_analog(value)
+		pass
+
 	def is_touched(self):
-		adc = ADC(Pin(self.num, Pin.IN))
-		adc.atten(ADC.ATTN_0DB)
-		return adc.read() == 4095
+		return read_analog == 4095
